70dcb62811e5c61dcf6ac61748842cad29b09553
Soujiro17 <vicente_tomi@hotmail.com>
2022-11-18 10:17:29 -0300

    editar registro hecho

--- src/components/RegistroEdit/index.jsx
+++ src/components/RegistroEdit/index.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { useOutletContext } from "react-router-dom";
 import Input from "../../shared/Input";
+import Label from "../../shared/Label";
 import Card from "../Card";
+import MapaLeaftlet from "../MapaLeaflet";
 import { Container, EditForm } from "./style";
 
 const RegistroEdit = () => {
@@ -10,8 +12,14 @@ const RegistroEdit = () => {
   return (
     <Container>
       <EditForm>
-        <Card width="100%">
-          <Input />
+        <Card width="100%" height="100%">
+          <MapaLeaftlet height="200px" width="100%" />
+          <Label htmlFor="direccion">Dirección</Label>
+          <Input id="direccion" value={value.direccion} width="100%" />
+          <Label htmlFor="reportadoPor">Reportado por</Label>
+          <Input id="reportadoPor" value={value.reportadoPor} width="100%" />
+          <Label htmlFor="fecha">Fecha</Label>
+          <Input type="date" id="fecha" value={value.fecha} width="100%" />
         </Card>
       </EditForm>
     </Container>
