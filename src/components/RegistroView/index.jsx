b4a2eb08934b7779cd283b214b10e782b44b45ee
Soujiro17 <vicente_tomi@hotmail.com>
2022-11-18 10:02:49 -0300

    ver registro hecho

--- src/components/RegistroView/index.jsx
+++ src/components/RegistroView/index.jsx
@@ -1,16 +1,49 @@
 import React from "react";
 import { useOutletContext } from "react-router-dom";
 import MapaLeaflet from "../MapaLeaflet";
-import { Container, DatosContainer, MapaContainer } from "./style";
+import Input from "../../shared/Input";
+import {
+  Container,
+  DatosContainer,
+  InputGroup,
+  InputLabel,
+  MapaContainer,
+} from "./style";
+import Label from "../../shared/Label";
 
 const RegistroView = () => {
   const { value } = useOutletContext();
 
   return (
     <Container>
-      <DatosContainer>{JSON.stringify(value)}</DatosContainer>
+      <DatosContainer>
+        <InputGroup>
+          <InputLabel>
+            <Label htmlFor="id">ID</Label>
+            <Input id="id" value={value.id} width="100%" disabled />
+          </InputLabel>
+          <InputLabel>
+            <Label htmlFor="activo">Activo</Label>
+            <Input id="activo" value={value.activo} width="100%" disabled />
+          </InputLabel>
+        </InputGroup>
+        <InputLabel>
+          <Label>Reportado por</Label>
+          <Input value={value.reportadoPor} width="100%" disabled />
+        </InputLabel>
+        <InputGroup>
+          <InputLabel>
+            <Label>Fecha</Label>
+            <Input value={value.fecha} width="100%" disabled />
+          </InputLabel>
+          <InputLabel>
+            <Label>Dirección</Label>
+            <Input value={value.direccion} width="100%" disabled />
+          </InputLabel>
+        </InputGroup>
+      </DatosContainer>
       <MapaContainer>
-        <MapaLeaflet punto={value} height="400px" width="100%" />
+        <MapaLeaflet punto={value} height="100%" width="100%" />
       </MapaContainer>
     </Container>
   );
--- src/components/RegistroView/style.jsx
+++ src/components/RegistroView/style.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  height: 100%;
+  height: 400px;
   width: 100%;
   display: flex;
   align-items: center;
@@ -11,9 +11,8 @@ export const Container = styled.div`
 
 export const DatosContainer = styled.div`
   display: flex;
-  align-items: center;
-  justify-content: center;
   flex-direction: column;
+  gap: 1rem;
   height: 100%;
   width: 49%;
 `;
@@ -24,4 +23,15 @@ export const MapaContainer = styled.div`
   justify-content: center;
   height: 100%;
   width: 49%;
+`;
+
+export const InputGroup = styled.div`
+  display: flex;
+  gap: 1%;
+`;
+
+export const InputLabel = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
 `;
